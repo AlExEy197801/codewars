e2ce05d9ef6c65bab960405d1c1f3da89edbea60
Алексей Трудов <atrudoff@yandex.by>
2023-12-08 09:29:26 +0300

    are-you-playing-banjo.js

---
+++ 8kyu/are-you-playing-banjo.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//
+// The function takes a name as its only argument, and returns one of the following strings:
+//
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+// SOLUTION:
+
+function areYouPlayingBanjo(name) {
+  // Implement me
+  if (name.startsWith("r") || name.startsWith("R")) {
+    name = name + " plays banjo"
+  } else {
+    name = name + " does not play banjo"
+  }
+  return name;
+}
+
+console.log(areYouPlayingBanjo("Adam")) // "Adam does not play banjo"
+console.log(areYouPlayingBanjo("Paul")) // "Paul does not play banjo"
+console.log(areYouPlayingBanjo("Ringo")) // "Ringo plays banjo"
+console.log(areYouPlayingBanjo("bravo")) // "bravo does not play banjo"
+console.log(areYouPlayingBanjo("rolf")) // "rolf plays banjo"
+
